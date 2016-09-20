1dc5a502cb2d353508fde4a98c3f1ccea4ef5e7a
yanghuayao <yanghuayao0530@163.com>
2016-09-20 15:35:04 +0800

    test

---
+++ webTest/src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA
+ * name：Lingce
+ * date：2016/9/20
+ * time：15:25
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String a = "a";
+
+    }
+}
