51e2f4c9ae89672bb80c3f18b866a9ca0c22987e
yanghuayao <yanghuayao0530@163.com>
2016-10-14 14:32:48 +0800

    test

--- webTest/src/com/yhy/service/ProductService.java
+++ webTest/src/com/yhy/service/ProductService.java
@@ -1,11 +1,18 @@
 package com.yhy.service;
 
+import com.yhy.dao.ProductDao;
+
 /**
  * Created with IntelliJ IDEA
  * author：yhy
  * date：  2016/10/14 13:58
  */
 public class ProductService {
-    
+
+	private ProductDao productDao;
+	public static void main(String[] args){
+
+	}
+
     //System.out.print("=============");
 }
